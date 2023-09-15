a658f26bcfd4ceba418bcc79eba5d873f3f0c2ea
ChrisSong <1352411860@qq.com>
2023-09-15 22:43:08 +0800

    style: 样式修改

--- components/layout.tsx
+++ components/layout.tsx
@@ -23,6 +23,7 @@ const Layout = ({ children }: Props) => {
             position: "fixed",
             top: 0,
             width: "100%",
+            zIndex: 100,
           }}
           className="blog-header"
         >
